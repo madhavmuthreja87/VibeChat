53ded06da1f03f35b6aa38d49ece600c8def49de
madhavmuthreja87 <muthrejamadhav@gmail.com>
2026-08-16 15:21:43 +0530

    App icon added and basic home page ui created

--- vibechat/lib/main.dart
+++ vibechat/lib/main.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Vibe Chat",
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          //Ek baar app bar ka theme decide krna padega bass
+          elevation: 1,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black,
+            fontSize: 19,
+          ),
+          iconTheme: IconThemeData(color: Colors.black),
+          backgroundColor: Colors.transparent,
+        ),
+      ),
+
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ vibechat/lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Vibe Chat"),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFFFDC70C), Color(0xFFF4933D), Color(0xFFE93E3A)],
+            ),
+          ),
+        ),
+        leading: Icon(Icons.home),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+        ],
+      ),
+
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10.0),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add_comment),
+        ),
+      ),
+    );
+  }
+}
